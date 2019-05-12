707ffc8f18bf93c46abad199bc0539d78e12f834
msjung <jms7446@gmail.com>
2019-05-12 15:41:55 +0900

    #1 add app lists and make first unittest

---
+++ lists/tests.py
@@ -0,0 +1,12 @@
+# from django.test import TestCase
+import pytest
+
+
+# class SmokeTest(TestCase):
+#
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+
+
+def test_first():
+    assert 1 + 1 == 2
