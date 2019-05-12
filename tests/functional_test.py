d5a78a0e778e3fcd3c6129afa0fc00b4cdee3f86
msjung <jms7446@gmail.com>
2019-05-12 20:23:56 +0900

    #1 functional test now checks we can input a to-do item

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,6 +1,9 @@
-from util import make_driver
+import time
 
+from selenium.webdriver.common.keys import Keys
 from pytest import fixture
+
+from util import make_driver
 
 
 @fixture
@@ -17,19 +20,30 @@ def test_can_start_a_list_and_retrieve_it_later(browser):
 
     # she notices the page title and header mention to-do lists
     print(browser.title)
-    assert 'To-do' in browser.title
-    raise Exception("Finish the test!!")
+    assert 'To-Do' in browser.title
+    header_text = browser.find_element_by_tag_name('h1').text
+    assert 'To-Do' in header_text
 
     # She is invited to enter a to-do item straight away
+    input_box = browser.find_element_by_id('id_new_item')
+    assert input_box.get_attribute('placeholder') == 'Enter a to-do item'
 
     # She types "Buy peacock feathers" into a text box
     # (Edith's hobby is tying fly-fishing lures)
+    input_box.send_keys('Buy peacock feathers')
 
     # When she hits enter, the page updates, and now the page lists
     # "1: Buy peacock feathers" as an item in a to-do list
+    input_box.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    assert any(row.text == '1: Buy peacock feathers' for row in rows)
 
     # There is still a text box inviting her to add another item.
     # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+    raise Exception('Finish the test!')
 
     # The page updates again, and now shows both items on her list
 
