c6fb5bd28c8839c83081631de69e41d162c6bfd2
msjung <jms7446@gmail.com>
2019-05-12 23:16:37 +0900

    #1 Front page HTML now generated from a template

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -39,7 +39,7 @@ def test_can_start_a_list_and_retrieve_it_later(browser):
 
     table = browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
-    assert any(row.text == '1: Buy peacock feathers' for row in rows)
+    assert any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
 
     # There is still a text box inviting her to add another item.
     # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
