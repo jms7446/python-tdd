cb016b2a05df1610b7ad42ddeb550e96d7a6a4c7
msjung <jms7446@gmail.com>
2019-05-12 16:00:16 +0900

    #1 First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,8 @@
 # from django.test import TestCase
+from django.urls import resolve
 import pytest
+
+from lists.views import home_page
 
 
 # class SmokeTest(TestCase):
@@ -8,5 +11,10 @@ import pytest
 #         self.assertEqual(1 + 1, 3)
 
 
-def test_first():
-    assert 1 + 1 == 2
+################################################################################
+# Homepage
+################################################################################
+
+def test_root_url_resolvers_to_home_page_view():
+    found = resolve('/')
+    assert found.func == home_page
