e75eb7bdb4965174fa7bd26799b387a3a2afa24e
msjung <jms7446@gmail.com>
2019-05-13 13:09:43 +0900

    #1 model for list items

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 
 # class SmokeTest(TestCase):
@@ -37,3 +38,28 @@ def test_can_save_a_post_request(client):
     response = client.post('/', data={'item_text': item_text})
     assert item_text in response.content.decode()
     assert is_template_used(response, 'home.html')
+
+
+################################################################################
+# ItemModel
+################################################################################
+
+@pytest.mark.django_db
+def test_saving_and_retrieving_items():
+    first_item = Item()
+    first_text = 'The first (ever) list item'
+    first_item.text = first_text
+    first_item.save()
+
+    second_item = Item()
+    second_text = 'Item the second'
+    second_item.text = second_text
+    second_item.save()
+
+    saved_items = Item.objects.all()
+    assert saved_items.count() == 2
+
+    first_saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+    assert first_saved_item.text == first_text
+    assert second_saved_item.text == second_text
